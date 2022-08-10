610c2ec64ab2c76536154af26156bde6890411fd
Doug Brown <doug@schmorgal.com>
2022-08-10 15:31:43 -0700

    spi-pxa2xx: increase the minimum DMA size
    The minimum DMA size is currently set to 1 byte. The overhead in setting
    up DMA isn't worth it for 1 byte, so increase it to something where DMA
    would start to make sense.

--- drivers/spi/spi-pxa2xx.c
+++ drivers/spi/spi-pxa2xx.c
@@ -1540,7 +1540,7 @@ pxa2xx_spi_init_pdata(struct platform_device *pdev)
 	pdata->is_slave = device_property_read_bool(&pdev->dev, "spi-slave");
 	pdata->num_chipselect = 1;
 	pdata->enable_dma = true;
-	pdata->dma_burst_size = 1;
+	pdata->dma_burst_size = 8;
 
 	return pdata;
 }
